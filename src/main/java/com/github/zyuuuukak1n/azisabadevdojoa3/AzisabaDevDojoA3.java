2f8e7fa40ba1d9477d1a6318c9afcc9b806ca113
zyuuuukak1n <ac.zyuuuukak1n@gmail.com>
2022-06-09 20:31:33 +0900

    first commit

---
+++ src/main/java/com/github/zyuuuukak1n/azisabadevdojoa3/AzisabaDevDojoA3.java
@@ -0,0 +1,17 @@
+package com.github.zyuuuukak1n.azisabadevdojoa3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AzisabaDevDojoA3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
